f33227c4ff4aa76733a89558b18259b7d1c0d99a
Zachary Seebeck <seebeckzachary@gmail.com>
2019-11-24 21:26:08 -0500

    Uploaded the dev files.

---
+++ src/components/Project.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="xl:max-w-5xl lg:max-w-3xl md:max-w-2xl sm:max-w-md w-full lg:flex mt-5">
+    <div class="h-48 lg:h-auto lg:w-64 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" v-if="img" :style="{ backgroundImage: 'url(' + img + ')' }" title="Woman holding a mug"></div>
+    <div class="border-r border-b border-l border-t border-accent lg:border-l-1 lg:border-t lg:border-accent bg-background rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+      <div class="mb-4">
+        <div class="text-white font-bold text-xl mb-2">{{ title }}</div>
+        <p class="text-white text-base">{{ desc }}</p>
+      </div>
+      <div v-if="git" class="mb-4 flex justify-center">
+        <a v-if="git" :href="git" class="btn" target="_new"><font-awesome-icon :icon="['fas', 'code']" /> Source Code</a>  
+        <a v-if="live" :href="live" class="btn" target="_new"><font-awesome-icon :icon="['fas', 'laptop']" /> Live Demo</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title', 'desc', 'img', 'git', 'live']
+}
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home content-center h-full mx-2">
+    <img class="h-64 w-64 mt-12 rounded-full mx-auto object-cover" src="/img/senior_wall.jpg">
+    <h1 class="text-primary text-3xl py-auto">Zachary Seebeck</h1>
+    <h4 class="text-secondary text-xl py-auto">A student, web developer, and musician located in Bay City, Michigan</h4>
+    <hr class="w-64 mx-auto mt-4" />
+    <div class="my-5">
+      <p class="text-secondary text-sm">seebeckzachary@gmail.com / (989) 750-5996</p>
+    </div>
+    <hr class="w-64 mx-auto mt-4" />
+    <div class="my-10">
+      <a href="https://github.com/ZacharySeebeck2020" target="new" class="social-btn"><font-awesome-icon :icon="['fab', 'github']" /></a>
+      <a href="https://www.linkedin.com/in/zacharyseebeck/" target="new" class="social-btn"><font-awesome-icon :icon="['fab', 'linkedin']" /></a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  components: {
+  }
+}
+</script>
