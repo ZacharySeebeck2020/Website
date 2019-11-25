48eab604541287e9724ff11e3c9ea4ad9f67d627
Zachary Seebeck <seebeckzachary@gmail.com>
2019-11-24 22:37:26 -0500

    Created a deploy script to automate uploading changes on the site.

---
+++ deploy.ps1
@@ -0,0 +1,24 @@
+Write-Host "Attempting to deploy" 
+# Build the Vue App
+npm run build
+
+# Change directory into the dist folder.
+cd dist
+
+# Add the CNAME file for custom domain support
+echo 'zacharyseebeck.com' > CNAME
+
+$message = Read-Host -Prompt 'Deploy message: '
+
+# Initialize and add everything to a new git repository
+git init
+git add -A
+git commit -m $message
+
+# Push to the GitHub repository.
+git push -f https://github.com/ZacharySeebeck2020/zacharyseebeck2020.github.io.git master
+
+# Go back to the starting directory.
+cd ..
+
+Write-Host "\n\n\nDeployed properly with message '$message'" 
